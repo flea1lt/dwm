1f36284e078f46c9509a7a01afadf2847899695b
fleashiro <fleashiro@gmail.com>
2023-05-03 00:56:08 +0800

    use JetBrainsMono Nerd Font Mono

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font Mono:size=18" };
-static const char dmenufont[]       = "Hack Nerd Font Mono:size=18";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=18" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=18";
 static unsigned int baralpha	    = 0xd0;
 static unsigned int borderalpha	    = OPAQUE;
 static const char col_gray1[]       = "#222222";
